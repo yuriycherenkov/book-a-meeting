392c9111c1e64f3b66a343d91639765f30f46df5
CherenkovYuriy <Yuriy.Cherenkov@contractor.cengage.com>
2023-02-28 12:42:17 +0100

    feat: add some validation

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,13 +1,25 @@
 import prisma from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
 import nc from 'next-connect';
 import { onError } from '../../utils/onError';
+import { authOptions } from './auth/[...nextauth]';
 
-const getUsersPrisma = () => prisma.user.findMany();
+const getUsersPrisma = (organizerId: number) =>
+  prisma.user.findMany({
+    where: {
+      NOT: {
+        id: organizerId,
+      },
+    },
+  });
 
 // GET /api/users
 const getUsers = async (req: NextApiRequest, res: NextApiResponse) => {
-  const response = await getUsersPrisma();
+  const session = await getServerSession(req, res, authOptions);
+  const organizerId = Number(session?.user?.id);
+
+  const response = await getUsersPrisma(organizerId);
 
   res.status(200).json(response);
 };
